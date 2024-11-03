17b909754833a5d3d4cb6b8ce2ec154de517c00d
Mateor13 <mateo.torres@epn.edu.ec>
2024-11-02 23:07:41 -0500

    Login

--- src/Login/assets/js/recognition.js
+++ src/Login/assets/js/recognition.js
@@ -3,15 +3,17 @@ const apiKey = "a8IXOGUopV8kvYThTywIQdp0tdz4jRPl";
         const apiUrl = "https://api-us.faceplusplus.com/facepp/v3/detect";
 
         async function detectFace() {
+            const advertencia = document.querySelector('p');
             const fileInput = document.getElementById("imageFile");
             const imageFile = fileInput.files[0];
 
             if (!imageFile) {
-                alert("Por favor, selecciona una imagen.");
+                advertencia.style.display = "block";
+                advertencia.style.color = "#ff4757";
+                advertencia.innerHTML = "Por favor, selecciona una imagen con tu rostro antes de continuar.";
                 return;
             }
 
-            // Crear el objeto FormData
             const formData = new FormData();
             formData.append("api_key", apiKey);
             formData.append("api_secret", apiSecret);
@@ -28,12 +30,20 @@ const apiKey = "a8IXOGUopV8kvYThTywIQdp0tdz4jRPl";
                 console.log("Respuesta de la API:", result);
 
                 if (result.faces && result.faces.length > 0) {
-                    window.location.href = "/Inicio";
+                    advertencia.style.display = "block";
+                    advertencia.style.color = "#ff4757";
+                    advertencia.innerHTML = "Detección exitosa. Redirigiendo...";
+                    setTimeout(() => {
+                    window.location.href = "/Inicio"}, 2000);
                 } else {
-                    alert("No se detectó ningún rostro en la imagen.");
+                    advertencia.style.display = "block";
+                    advertencia.style.color = "#ff4757";
+                    advertencia.innerHTML = "No se detectó un rostro en la imagen. Por favor, intenta de nuevo.";
                 }
             } catch (error) {
                 console.error("Error en la solicitud:", error);
-                alert("Ocurrió un error al intentar detectar el rostro.");
+                advertencia.style.display = "block";
+                advertencia.style.color = "#ff4757";
+                advertencia.innerHTML = "Ocurrió un error al intentar detectar el rostro.";
             }
         }
